741989c72f3c0a6bbc748b42c41262063af50eb9
Himanshu Yadav <himanshu.yadav@syngenta.com>
2024-02-23 00:11:50 +0530

    Aligned

--- app/workflow/jobs/bydv.py
+++ app/workflow/jobs/bydv.py
@@ -10,6 +10,7 @@ from app.workflow.jobs.base_job import BaseJob
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class Bydv(BaseJob):
     BYDV_REQUEST = {
         "request_version": "v1.0",
