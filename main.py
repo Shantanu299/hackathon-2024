6855bd8e21ee1aef0d3b4327f3e69695ba72469e
shantanupatil29 <shantanu_eknath.patil@syngenta.com>
2024-02-23 12:19:47 +0530

    CORS issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import uvicorn
 from dotenv import load_dotenv; load_dotenv()
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.routers import wallace
 
@@ -8,6 +9,18 @@ app = FastAPI(title="Start Hack", description="Hackathon Application", version="
 
 logger = logging.getLogger(__name__)
 logger.level = logging.INFO
+
+CORS_ORIGINS = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(wallace.router)
 
